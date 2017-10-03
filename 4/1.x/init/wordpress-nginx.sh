a635bf132013c7557b622fae12e11bde338af50c
Chingis Sandanov <chingis@wodby.com>
2017-10-03 20:27:41 +0700

    Set ownership on mounted dirs to www-data

--- 4/1.x/init/wordpress-nginx.sh
+++ 4/1.x/init/wordpress-nginx.sh
@@ -6,5 +6,7 @@ if [[ -n "${DEBUG}" ]]; then
     set -x
 fi
 
+sudo fix-permissions.sh www-data www-data "${HTML_DIR}" "${WODBY_DIR_FILES}"
+
 gotpl /etc/gotpl/fastcgi_params.tpl > /etc/nginx/fastcgi_params
 gotpl /etc/gotpl/wordpress.conf.tpl > /etc/nginx/conf.d/wordpress.conf
